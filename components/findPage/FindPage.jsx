ab4488de46e4ece24a49fab0a212ffc30831c90b
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-05-25 23:39:12 +0300

    finaly found some logic in nextJS image

--- components/findPage/FindPage.jsx
+++ components/findPage/FindPage.jsx
@@ -1,31 +1,31 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from 'react-redux';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import { faX } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import ObjectCard from "./objectCard/ObjectCard";
+import ObjectCard from './objectCard/ObjectCard';
 
-import PageBlur from "../wholePageBlur/PageBlur";
-import ModalMeinMenu from "../modalMainMenu/ModalMainMenu";
-import ModalFind from "../modalFind/ModalFind";
+// import PageBlur from '../wholePageBlur/PageBlur';
+// import ModalMeinMenu from '../modalMainMenu/ModalMainMenu';
+// import ModalFind from '../modalFind/ModalFind';
 
-import style from "./FindPage.module.scss";
+import style from './FindPage.module.scss';
 
 export default function Find() {
-  const [filterInputValue, setFilterInputValue] = useState("");
+  const [filterInputValue, setFilterInputValue] = useState('');
   const [showXIcon, setShowXIcon] = useState(false);
 
-  const filterInputHandler = () => {
-    setFilterInputValue(target.input.value);
-    if (target.input.value.trim().length > 0) {
+  const filterInputHandler = (event) => {
+    setFilterInputValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
       setShowXIcon(true);
     }
   };
 
   const xCloseHandler = () => {
-    setFilterInputValue("");
+    setFilterInputValue('');
     setShowXIcon(false);
   };
 
@@ -35,7 +35,7 @@ export default function Find() {
         <div className={style.imitateBigInput}>
           <button
             type="button"
-            onClick={() => console.log("click")}
+            onClick={() => console.log('click')}
             className={style.textButton}
           >
             m2/price
@@ -56,10 +56,10 @@ export default function Find() {
         </div>
         <div>
           <h2>Оренда CHANGEFROMLINK в Білій Церкві</h2>
-          <button>Усі</button>
-          <button>Піонерська</button>
-          <button>Вокзальна</button>
-          <button>Леваневського</button>
+          <button type="button">Усі</button>
+          <button type="button">Піонерська</button>
+          <button type="button">Вокзальна</button>
+          <button type="button">Леваневського</button>
         </div>
       </section>
       <div className={style.objectsCardsContainer}>
--- components/findPage/objectCard/ObjectCard.jsx
+++ components/findPage/objectCard/ObjectCard.jsx
@@ -1,23 +1,38 @@
-import style from './ObjectCard.module.scss'
+import Image from 'next/image';
+
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import style from './ObjectCard.module.scss';
 
 export default function ObjectCard() {
-    return (
-        <div>
-            <div className={style.imgContainer}>
-                <img />
-            </div>
-            <div className={style.descriptionContainer}>
-            <h2>Адреса об'єкта</h2>
-            <p>Додатковий опис, який відображується лише після нажимання на +</p>
-            </div>
-            <div className={style.priceContainer}>
-                <h3>Тип - Офіс</h3>
-                <p>Ціна в місяць</p>
-                <p>Ціна за м2</p>
-            </div>
-            <button>
-                Детальніше
-            </button>
+  return (
+    <div className={style.wholeCard}>
+      <div className={style.imgContainer}>
+        <Image
+          src="https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+          alt="work"
+          style={{ objectFit: 'cover' }}
+          fill
+          priority
+          // width={100}
+          // height={100}
+        />
+      </div>
+      <div className={style.descriptionContainer}>
+        <div className={style.descriptionHeaderIcons}>
+          <h2>Вулиця Січневий Прорив 38А, Біла Церква, 09100</h2>
+          <FontAwesomeIcon icon={faPlus} className={style.iconsDescription} />
+          {/* <FontAwesomeIcon icon={faMinus} />  className={style.iconsDescription} */}
         </div>
-    )
+        <p>Додатковий опис, який відображується лише після нажимання на +</p>
+      </div>
+      <div className={style.priceContainer}>
+        <h3>Тип - Офіс</h3>
+        <p>Ціна в місяць</p>
+        <p>Ціна за м2</p>
+      </div>
+      <button type="button">Детальніше</button>
+    </div>
+  );
 }
