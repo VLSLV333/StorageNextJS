ed7b053723a20afa4e5d6934af3a4f6fe7478eda
Micle Dev <mickle.develop@gmail.com>
2023-05-31 19:04:32 +0300

    2 clicks today! Unfortunatelly can't speed up now

--- components/findPage/FindPage.jsx
+++ components/findPage/FindPage.jsx
@@ -1,52 +1,52 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from "react";
 
-import { faX } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import ObjectCard from './objectCard/ObjectCard';
+import ObjectCard from "./objectCard/ObjectCard";
 
-import PageBlur from '../wholePageBlur/PageBlur';
-import ModalMeinMenu from '../modalMainMenu/ModalMainMenu';
-import ModalPhone from '../modalPhone/ModalPhone';
+import PageBlur from "../wholePageBlur/PageBlur";
+import ModalMeinMenu from "../modalMainMenu/ModalMainMenu";
+import ModalPhone from "../modalPhone/ModalPhone";
 
-import ArrowDown from '../mainPage/main/vectors/arrowDown/ArrowDown';
-import ArrowUp from '../mainPage/main/vectors/arrowUp/ArrowUp';
+import ArrowDown from "../mainPage/main/vectors/arrowDown/ArrowDown";
+import ArrowUp from "../mainPage/main/vectors/arrowUp/ArrowUp";
 
-import style from './FindPage.module.scss';
+import style from "./FindPage.module.scss";
 
 const rentObjectsFromDB = [
   {
-    link: 'https://images.unsplash.com/photo-1685371863623-effd71822cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+    link: "https://images.unsplash.com/photo-1685371863623-effd71822cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
     m2: 20,
-    location: 'pion',
+    location: "pion",
     price: 1000,
-    exactAddres: 'Вулиця Січневий Прорив 38А, Біла Церква, 09100',
+    exactAddres: "Вулиця Січневий Прорив 38А, Біла Церква, 09100",
     description:
-      'Приємний ремонт / Закрита територія / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація / Кондиціонер',
+      "Приємний ремонт / Закрита територія / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація / Кондиціонер",
     id: 0,
   },
   {
-    link: 'https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    link: "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     m2: 100,
-    location: 'vokz',
+    location: "vokz",
     price: 3000,
-    exactAddres: '2-ий Партизанський провулок 31, Біла Церква, 09100',
+    exactAddres: "2-ий Партизанський провулок 31, Біла Церква, 09100",
     description:
-      '2 поверх / Будівля спортовно-оздоровчого комплексу / Цілодобова охорона / Приміщення правильної форми / Прекрасний колектив / Електроенергія / Каналізація / Кондиціонер',
+      "2 поверх / Будівля спортовно-оздоровчого комплексу / Цілодобова охорона / Приміщення правильної форми / Прекрасний колектив / Електроенергія / Каналізація / Кондиціонер",
     id: 1,
   },
   {
-    link: 'https://images.unsplash.com/photo-1685443866545-57adcff6e0be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80',
+    link: "https://images.unsplash.com/photo-1685443866545-57adcff6e0be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
     m2: 200,
-    location: 'levan',
+    location: "levan",
     price: 7000,
-    exactAddres: 'Вулиця Леваневського 85, Біла Церква, 09100',
+    exactAddres: "Вулиця Леваневського 85, Біла Церква, 09100",
     description:
-      'Закрита територія / Рампа / Приміщення різних розмірів / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація',
+      "Закрита територія / Рампа / Приміщення різних розмірів / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація",
     id: 2,
   },
 ];
@@ -56,7 +56,7 @@ export default function Find() {
 
   const router = useRouter();
   const {
-    query: { whatIsRented },
+    query: { whatIsRented, size },
   } = router;
 
   const openBugerMenu = useSelector((state) => state.burgerMenu.openBurgerMenu);
@@ -66,38 +66,28 @@ export default function Find() {
   );
 
   const [greenBorder, setGreenBorder] = useState(false);
-  const [selectedButtonFilter, setSelectedButtonFilter] = useState('all');
+  const [selectedButtonFilter, setSelectedButtonFilter] = useState("all");
 
-  const [filterInputValue, setFilterInputValue] = useState('');
+  const [filterInputValue, setFilterInputValue] = useState(size || "");
   const [showXIcon, setShowXIcon] = useState(false);
 
-  const [visibleFilterOption, setVisibleFilterOption] = useState('m2');
+  const [visibleFilterOption, setVisibleFilterOption] = useState("m2");
   const [alternativeFilterOption, setAlternativeFilterOption] =
-    useState('ціна');
+    useState("ціна");
   const [openedFilterModal, setFilterModal] = useState(false);
 
   const [objectsForPage, setObjectsForPage] = useState(rentObjectsFromDB);
+
+  const [arrayFilteredWithButton, setArrayFilteredWithButton] =
+    useState(rentObjectsFromDB);
 
   const filterInputHandler = (event) => {
     setFilterInputValue(event.target.value);
     if (event.target.value.trim().length > 0) {
       setShowXIcon(true);
     }
-    if (visibleFilterOption === 'm2') {
-      const filteredArray = rentObjectsFromDB.filter(
-        (obj) => obj.m2 >= event.target.value
-      );
-      setObjectsForPage(filteredArray);
-    } else if (visibleFilterOption === 'ціна') {
-      const filteredArray = rentObjectsFromDB.filter(
-        (obj) => obj.price >= event.target.value
-      );
-      setObjectsForPage(filteredArray);
-    }
-
     if (event.target.value.trim().length === 0) {
       setShowXIcon(false);
-      setObjectsForPage(rentObjectsFromDB);
     }
   };
 
@@ -109,7 +99,7 @@ export default function Find() {
   };
 
   const xCloseHandler = () => {
-    setFilterInputValue('');
+    setFilterInputValue("");
     setShowXIcon(false);
   };
 
@@ -117,8 +107,6 @@ export default function Find() {
     setAlternativeFilterOption(visibleFilterOption);
     setVisibleFilterOption(e.target.innerText);
     setFilterModal(false);
-    setFilterInputValue('');
-    setObjectsForPage(rentObjectsFromDB);
   };
 
   const visibleFilterButtonHandler = () => {
@@ -126,39 +114,60 @@ export default function Find() {
   };
 
   const dynamicPlaceHolder =
-    visibleFilterOption === 'm2' ? 'введіть квадратуру' : 'введіть ціну';
+    visibleFilterOption === "m2" ? "введіть квадратуру" : "введіть ціну";
 
   const filterAreaButtonsHandler = (e) => {
     setSelectedButtonFilter(e.target.id);
-    if (e.target.id === 'all') {
-      setObjectsForPage(rentObjectsFromDB);
+    if (e.target.id === "all") {
+      setArrayFilteredWithButton(rentObjectsFromDB);
       return;
     }
-    const filteredArray = rentObjectsFromDB.filter(
+    let filteredArray = rentObjectsFromDB.filter(
       (obj) => obj.location === e.target.id
     );
-    setObjectsForPage(filteredArray);
+    setArrayFilteredWithButton(filteredArray);
   };
 
-  const selectedAreaButton =
-    selectedButtonFilter === 'all'
-      ? 1
-      : selectedButtonFilter === 'pion'
-        ? 2
-        : selectedButtonFilter === 'vokz'
-          ? 3
-          : selectedButtonFilter === 'levan'
-            ? 4
-            : '';
+  useEffect(() => {
+    if (filterInputValue > 0 && filterInputValue.trim().length > 0) {
+      if (visibleFilterOption === "m2") {
+        const filteredArray = arrayFilteredWithButton.filter(
+          (obj) => obj.m2 >= filterInputValue
+        );
+        setObjectsForPage(filteredArray);
+      } else if (visibleFilterOption === "ціна") {
+        const filteredArray = arrayFilteredWithButton.filter(
+          (obj) => obj.price >= filterInputValue
+        );
+        setObjectsForPage(filteredArray);
+      }
+      setShowXIcon(true);
+    } else {
+      setObjectsForPage(arrayFilteredWithButton);
+    }
+  }, [filterInputValue, arrayFilteredWithButton, visibleFilterOption]);
 
-  const textForCardHeading = whatIsRented === 'Офіси' ? 'Офіс' : '';
+  const selectedAreaButton =
+    selectedButtonFilter === "all"
+      ? 1
+      : selectedButtonFilter === "pion"
+      ? 2
+      : selectedButtonFilter === "vokz"
+      ? 3
+      : selectedButtonFilter === "levan"
+      ? 4
+      : "";
+
+  const textForCardHeading = whatIsRented === "Офіси" ? "Офіс" : "";
+
+  const noObjectsFound = objectsForPage.length === 0;
 
   return (
     <>
       <section className={style.textAndFindContainer}>
         <div
           className={`${style.imitateBigInput} ${
-            greenBorder ? style.greenBorder : ''
+            greenBorder ? style.greenBorder : ""
           }`}
         >
           <button
@@ -200,43 +209,59 @@ export default function Find() {
         </div>
         <div className={style.heddingAndButtons}>
           <h2>{whatIsRented} в Білій Церкві</h2>
-          <div className={style.buttonsContainer}>
-            <button
-              type="button"
-              className={`${selectedAreaButton === 1 ? style.buttonGreen : ''}`}
-              id="all"
-              onClick={filterAreaButtonsHandler}
-            >
-              Усі
-            </button>
-            <button
-              type="button"
-              id="pion"
-              onClick={filterAreaButtonsHandler}
-              className={`${selectedAreaButton === 2 ? style.buttonGreen : ''}`}
-            >
-              Піонерська
-            </button>
-            <button
-              type="button"
-              id="vokz"
-              onClick={filterAreaButtonsHandler}
-              className={`${selectedAreaButton === 3 ? style.buttonGreen : ''}`}
-            >
-              Вокзальна
-            </button>
-            <button
-              type="button"
-              className={`${style.lastButton} ${
-                selectedAreaButton === 4 ? style.buttonGreen : ''
-              }`}
-              id="levan"
-              onClick={filterAreaButtonsHandler}
-            >
-              Леваневського
-            </button>
+          <div className={style.buttonsAndFilterResult}>
+            <div className={style.buttonsContainer}>
+              <button
+                type="button"
+                className={`${
+                  selectedAreaButton === 1 ? style.buttonGreen : ""
+                }`}
+                id="all"
+                onClick={filterAreaButtonsHandler}
+              >
+                Усі
+              </button>
+              <button
+                type="button"
+                id="pion"
+                onClick={filterAreaButtonsHandler}
+                className={`${
+                  selectedAreaButton === 2 ? style.buttonGreen : ""
+                }`}
+              >
+                Піонерська
+              </button>
+              <button
+                type="button"
+                id="vokz"
+                onClick={filterAreaButtonsHandler}
+                className={`${
+                  selectedAreaButton === 3 ? style.buttonGreen : ""
+                }`}
+              >
+                Вокзальна
+              </button>
+              <button
+                type="button"
+                className={`${style.lastButton} ${
+                  selectedAreaButton === 4 ? style.buttonGreen : ""
+                }`}
+                id="levan"
+                onClick={filterAreaButtonsHandler}
+              >
+                Леваневського
+              </button>
+            </div>
+            <div className={style.textFilterResult}>
+              <p>
+                <span className={style.borderForText}>
+                  Всього знайдено приміщень: {objectsForPage.length}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
+
         {openedFilterModal && (
           <div className={style.filterModal}>
             <button
@@ -251,18 +276,27 @@ export default function Find() {
         )}
       </section>
       <div className={style.objectsCardsContainer}>
-        {objectsForPage.map((object) => (
-          <ObjectCard
-            key={object.id}
-            link={object.link}
-            m2={object.m2}
-            price={object.price}
-            exactAddres={object.exactAddres}
-            description={object.description}
-            heading={textForCardHeading}
-          />
-        ))}
+        {!noObjectsFound &&
+          objectsForPage.map((object) => (
+            <ObjectCard
+              key={object.id}
+              link={object.link}
+              m2={object.m2}
+              price={object.price}
+              exactAddres={object.exactAddres}
+              description={object.description}
+              heading={textForCardHeading}
+            />
+          ))}
       </div>
+      {noObjectsFound && (
+        <div className={style.NoObjectsFound}>
+          <p>
+            Нажаль, приміщень не знайдено. <br />
+            Спробуйте змінити фільтр
+          </p>
+        </div>
+      )}
       {openBlur && <PageBlur />}
       {openBugerMenu && <ModalMeinMenu />}
       {openPhoneModal && <ModalPhone />}
