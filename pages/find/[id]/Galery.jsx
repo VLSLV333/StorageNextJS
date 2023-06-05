db5559c3053f2a64c62a7cce463ad86e7900a1b3
Micle Dev <mickle.develop@gmail.com>
2023-06-05 19:49:15 +0300

    3 lines today. mb will work more home

---
+++ pages/find/[id]/Galery.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+import style from "./Galery.module.scss";
+
+const Galery = ({ photosArray }) => {
+  return (
+    <div className={style.productCarousel}>
+      <div className={style.productContainerMobile}>
+        {photosArray?.map((photo, photoIndex) => (
+          <Image
+            src={photo}
+            alt="work"
+            style={{ objectFit: "cover" }}
+            height={375}
+            width={290}
+            // priority={photoIndex === 0}
+            key={photo}
+          />
+        ))}
+      </div>
+      <div className={style.productContainerTablet}>
+        {photosArray?.map((photo, photoIndex) => (
+          <Image
+            src={photo}
+            alt="work"
+            style={{ objectFit: "cover" }}
+            height={435}
+            width={395}
+            // priority={photoIndex === 0 || photoIndex === 1}
+            key={photo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Galery;
--- pages/find/[id]/index.jsx
+++ pages/find/[id]/index.jsx
@@ -1,53 +1,58 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Image from 'next/image';
+import Galery from "./Galery";
 
-import { useRouter } from 'next/router';
+import Image from "next/image";
 
-// import testBlur from '../../../images/mainPhoto/blurPre.webp';
+import { useRouter } from "next/router";
 
-import style from './objectDetails.module.scss';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import style from "./objectDetails.module.scss";
 
 const rentObjectsFromDB = [
   {
-    link: 'https://images.unsplash.com/photo-1685371863623-effd71822cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+    link: "https://images.unsplash.com/photo-1685371863623-effd71822cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
     m2: 20,
-    location: 'pion',
+    location: "pion",
     price: 1000,
-    exactAddres: 'Вулиця Січневий Прорив 38А, Біла Церква, 09100',
+    exactAddres: "Вулиця Січневий Прорив 38А, Біла Церква, 09100",
     description:
-      'Приємний ремонт / Закрита територія / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація / Кондиціонер',
+      "Приємний ремонт / Закрита територія / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація / Кондиціонер",
     id: 0,
     photos: [
-      'https://images.unsplash.com/photo-1682687220199-d0124f48f95b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-      'https://images.unsplash.com/photo-1685392198162-c110bc28e838?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1130&q=80',
-      'https://images.unsplash.com/photo-1674574124473-e91fdcabaefc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-      'https://images.unsplash.com/photo-1684868264391-a6dfb89882dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80',
-      'https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80',
+      "https://images.unsplash.com/photo-1682687220199-d0124f48f95b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+      "https://images.unsplash.com/photo-1685392198162-c110bc28e838?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1130&q=80",
+      "https://images.unsplash.com/photo-1674574124473-e91fdcabaefc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+      "https://images.unsplash.com/photo-1684868264391-a6dfb89882dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80",
+      "https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
     ],
   },
   {
-    link: 'https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    link: "https://images.unsplash.com/photo-1684779847639-fbcc5a57dfe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     m2: 100,
-    location: 'vokz',
+    location: "vokz",
     price: 3000,
-    exactAddres: '2-ий Партизанський провулок 31, Біла Церква, 09100',
+    exactAddres: "2-ий Партизанський провулок 31, Біла Церква, 09100",
     description:
-      '2 поверх / Будівля спортовно-оздоровчого комплексу / Цілодобова охорона / Приміщення правильної форми / Прекрасний колектив / Електроенергія / Каналізація / Кондиціонер',
+      "2 поверх / Будівля спортовно-оздоровчого комплексу / Цілодобова охорона / Приміщення правильної форми / Прекрасний колектив / Електроенергія / Каналізація / Кондиціонер",
     id: 1,
   },
   {
-    link: 'https://images.unsplash.com/photo-1685443866545-57adcff6e0be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80',
+    link: "https://images.unsplash.com/photo-1685443866545-57adcff6e0be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
     m2: 200,
-    location: 'levan',
+    location: "levan",
     price: 7000,
-    exactAddres: 'Вулиця Леваневського 85, Біла Церква, 09100',
+    exactAddres: "Вулиця Леваневського 85, Біла Церква, 09100",
     description:
-      'Закрита територія / Рампа / Приміщення різних розмірів / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація',
+      "Закрита територія / Рампа / Приміщення різних розмірів / Цілодобова охорона / Зона паркування / Асфальтована територія / Електроенергія / Каналізація",
     id: 2,
   },
 ];
-//  image 375px height
 
 export default function FindDynamic() {
   const router = useRouter();
@@ -56,54 +61,65 @@ export default function FindDynamic() {
   const {
     query: { id: thisObjectIdInDB },
   } = router;
-  // const thisObject = rentObjectsFromDB.filter(
-  //   (obj) => obj.id === +thisObjectIdInDB
-  // )[0];
 
-  const arrayWithImageElements = rentObjectsFromDB[thisObjectIdInDB]?.photos.map((photo) => (
-    <Image
-      src={photo}
-      // src={thisObject?.photos[photoIndex]}
-      alt="work"
-      style={{ objectFit: 'cover' }}
-      fill
-      priority
-      // placeholder="blur"
-      // blurDataURL={'"../../../images/mainPhoto/test.jpg"'}
-    />
-  )) || [];
+  const thisObjectsPhotosArray =
+    rentObjectsFromDB[thisObjectIdInDB]?.photos || [];
 
-  // console.log(thisObjectIdInDB)
-  // console.log(thisObject)
-  // <img src={thisObject.link} alt="test" />
-  // <p>{thisObject.description}</p>
-  const buttonHandler = () => {
+  const rightArrowHandler = () => {
     setPhotoIndexInArray((state) => {
-      if (state === 4) {
+      if (state === thisObjectsPhotosArray.length - 1) {
         return 0;
       }
-      state += 1;
-      return state;
+      return (state += 1);
     });
   };
+
+  const leftArrowHandler = () => {
+    setPhotoIndexInArray((state) => {
+      if (state === 0) {
+        return thisObjectsPhotosArray.length - 1;
+      }
+      return (state -= 1);
+    });
+  };
+
   return (
     <>
-      <div className={style.imageContainer}>
-        {/* <Image
-          src={thisObject ? thisObject?.photos[photoIndex] : testBlur}
-          // src={thisObject?.photos[photoIndex]}
-          alt="work"
-          style={{ objectFit: 'cover' }}
-          fill
-          priority
-          placeholder="blur"
-          blurDataURL={'"../../../images/mainPhoto/test.jpg"'}
-        /> */}
-        {arrayWithImageElements[photoIndexInArray]}
-      </div>
-      <button onClick={buttonHandler} type="button">
-        button!
-      </button>
+      <section className={style.sectionMainContainer}>
+        <Galery photosArray={thisObjectsPhotosArray} />
+        <div className={style.imagesAndModalElementsContainer}>
+          <div className={style.allImagesContainer}>
+            {rentObjectsFromDB[thisObjectIdInDB]?.photos.map(
+              (photo, photoIndex) => {
+                return (
+                  <div
+                    key={photo}
+                    className={`${style.imageContainer} ${
+                      photoIndexInArray === photoIndex
+                        ? style.relative
+                        : style.absolute
+                    }`}
+                  >
+                    <Image
+                      src={photo}
+                      alt="work"
+                      style={{ objectFit: "cover" }}
+                      fill
+                      priority={photoIndex === 0}
+                    />
+                  </div>
+                );
+              }
+            )}
+          </div>
+          <button className={style.arrowLeft} onClick={leftArrowHandler}>
+            <FontAwesomeIcon icon={faChevronLeft} className={style.xIcon} />
+          </button>
+          <button className={style.arrowRight} onClick={rightArrowHandler}>
+            <FontAwesomeIcon icon={faChevronRight} className={style.xIcon} />
+          </button>
+        </div>
+      </section>
     </>
   );
 }
